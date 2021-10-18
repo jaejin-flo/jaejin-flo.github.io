3a335c8dc6b0722ed69ddb20dd6387c520646ea9
jaejin-flo <jaejin@dreamus.io>
2021-10-18 03:46:47 +0000

    update

---
+++ firebase_init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDmv1fMHm8dnhAbBQoL5YNgsjGq2ML2QsU",
+    authDomain: "dodream-7229a.firebaseapp.com",
+    projectId: "dodream-7229a",
+    storageBucket: "dodream-7229a.appspot.com",
+    messagingSenderId: "482738846200",
+    appId: "1:482738846200:web:b91f87e1df59cd07783e0d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
